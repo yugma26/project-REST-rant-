7dc2654b369da195f80a77b7b0c898c3ccbc1c0f
yugma26 <66039476+yugma26@users.noreply.github.com>
2022-11-16 22:57:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 page</h1>')
+})
+
+app.listen(process.env.PORT)
