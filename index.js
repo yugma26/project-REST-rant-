36cb69a61096301e23f2a47b74166fb2334e81af
Yugma Prajapati <yugmaprajapati0210@gmail.com>
2022-11-19 12:37:41 -0500

    adding default view for layout

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('places',require('./controllers/places'))
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 page</h1>')
+    res.render('error404')
 })
 
-
+// Listen for Connections
 app.listen(process.env.PORT)
