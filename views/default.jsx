ad9788db3e7c48cbb13b8cf597b87602fb341ca6
Yugma Prajapati <yugmaprajapati0210@gmail.com>
2022-11-21 18:28:23 -0500

    adding view

--- views/default.jsx
+++ views/default.jsx
@@ -6,11 +6,7 @@ function Def (html) {
     return (
         <html>
             <head>
-                <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"/>
-                <a href="/places">
-                <button className="btn-primary">Places Page</button>
-                </a>
+                <title>Title</title>      
             </head>
             <body>
                 {html.children}
