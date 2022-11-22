d9d4c625009ac2ae8fc5b6040cfd14676861463f
Yugma Prajapati <yugmaprajapati0210@gmail.com>
2022-11-21 19:19:41 -0500

    adding index

--- index.js
+++ index.js
@@ -20,5 +20,25 @@ app.get('*', (req, res) => {
     res.render('404 page')
 })
 
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render(res.render('places/index', { places })
+    )
+  })
+  
 // Listen for Connections
 app.listen(process.env.PORT)
