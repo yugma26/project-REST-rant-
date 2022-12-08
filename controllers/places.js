0ebbd093263ef075c7c9769a21bf2000ff1ddcc5
Yugma Prajapati <yugmaprajapati0210@gmail.com>
2022-12-07 19:00:30 -0500

    form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -25,4 +25,9 @@ router.get('/', (req, res) => {
     res.render('places/new')
   })
   
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+  })
+  
 module.exports = router
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.static('public'))
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('home')
