31aee64418dd8ef3831b80268fd14d037e4c8322
Yugma Prajapati <yugmaprajapati0210@gmail.com>
2022-12-11 15:53:07 -0500

    adding the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -25,6 +25,20 @@ const places = require('../models/places.js')
     }
   })
   
+  router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+  })
+  
+ 
   router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
