600957c1436b2f417473d0b1a122fc3b9f432c10
Yugma Prajapati <yugmaprajapati0210@gmail.com>
2022-12-12 21:52:48 -0500

    adding year

--- models/places.js
+++ models/places.js
@@ -1,13 +1,14 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/photo-1551218808-94e220e084d2.avif'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/photo-1509395062183-67c5ad6faff9.avif'
-}]
+
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
