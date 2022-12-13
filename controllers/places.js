f806675c5cc5816ad10874af882a599f12d9edd6
Yugma Prajapati <yugmaprajapati0210@gmail.com>
2022-12-12 19:19:43 -0500

    adding the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -26,6 +26,19 @@ const places = require('../models/places.js')
   })
   
   router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] , id })
+  }
+})
+
+  router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
         res.render('error404')
@@ -34,7 +47,20 @@ const places = require('../models/places.js')
         res.render('error404')
     }
     else {
-      res.render('places/edit', { place: places[id] })
+      res.render('places/show', { place: places[id] , id})
+    }
+  })
+  
+  router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.redirect(`/places/${id}`)
     }
   })
   
@@ -53,6 +79,33 @@ const places = require('../models/places.js')
     }
     places.push(req.body)
     res.redirect('/places')
+  })
+  
+  router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+  
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
   })
   
 module.exports = router
